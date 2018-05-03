f1b77ac054d0ced3c245863a44d0839190a3dbf1
izi <krzysztof.suchomski@gmail.com>
2018-05-03 09:16:05 +0200

    Adding game of life with tests

--- src/game-of-life.ts
+++ src/game-of-life.ts
@@ -1,92 +1,106 @@
-export enum Life { Dead, Alive }
+export enum Life {
+  Dead,
+  Alive,
+}
 
 export class GameOfLife {
-    board: Life[][];
-    infiniteSpace = true;
+  public board: Life[][];
+  public infiniteSpace = true;
 
-    constructor(public height: number, public width: number, ...elements: Array<[number, number]>) {
-        this.board = this.empty();
+  constructor(
+    public height: number,
+    public width: number,
+    ...elements: Array<[number, number]>
+  ) {
+    this.board = this.empty();
 
-        if (elements) {
-            elements.forEach(e => {
-                const [row, cell] = e;
-                this.board[row][cell] = Life.Alive;
-            });
+    if (elements) {
+      elements.forEach(e => {
+        const [row, cell] = e;
+        this.board[row][cell] = Life.Alive;
+      });
+    }
+  }
+
+  public next() {
+    const newBoard = this.empty();
+
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        newBoard[i][j] = this.nextCellState(i, j);
+      }
+    }
+
+    this.board = newBoard;
+  }
+
+  public liveNeighbours(row: number, col: number) {
+    let liveNeighbours = 0;
+
+    for (let i: number = row - 1; i <= row + 1; i++) {
+      for (let j: number = col - 1; j <= col + 1; j++) {
+        if (!(i === row && j === col)) {
+          if (this.infiniteSpace) {
+            const r = this.indexOnPlane(i, this.height);
+            const c = this.indexOnPlane(j, this.width);
+            liveNeighbours += this.board[r][c];
+          } else if (this.isWithinPlane(i, j)) {
+            liveNeighbours += this.board[i][j];
+          }
         }
+      }
     }
 
-    next() {
-        const newBoard = this.empty();
+    return liveNeighbours;
+  }
 
-        for (let i = 0; i < this.height; i++) {
-            for (let j = 0; j < this.width; j++) {
-                newBoard[i][j] = this.nextCellState(i, j);
-            }
-        }
+  public empty() {
+    const board: Life[][] = new Array(this.height);
 
-        this.board = newBoard;
+    for (let i = 0; i < this.height; i++) {
+      board[i] = new Array<Life>(this.width).fill(Life.Dead);
     }
 
-    private nextCellState(i: number, j: number) {
-        const liveNeighbours = this.liveNeighbours(i, j);
-        const currentCellState = this.board[i][j];
+    return board;
+  }
 
-        if (this.isHealthyCell(currentCellState, liveNeighbours)) {
-            return Life.Alive;
-        } else if (this.isNewLifeCell(currentCellState, liveNeighbours)) {
-            return Life.Alive;
-        }
+  public clear() {
+    this.board = this.empty();
+  }
 
-        return Life.Dead;
+  private nextCellState(i: number, j: number) {
+    const liveNeighbours = this.liveNeighbours(i, j);
+    const currentCellState = this.board[i][j];
+
+    if (this.isHealthyCell(currentCellState, liveNeighbours)) {
+      return Life.Alive;
+    } else if (this.isNewLifeCell(currentCellState, liveNeighbours)) {
+      return Life.Alive;
     }
 
-    private isNewLifeCell(currentCellState: Life, liveNeighbours: number) {
-        return currentCellState === Life.Dead && liveNeighbours === 3;
-    }
+    return Life.Dead;
+  }
 
-    private isHealthyCell(currentCellState: Life, liveNeighbours: number) {
-        return currentCellState === Life.Alive && (liveNeighbours === 2 || liveNeighbours === 3);
-    }
+  private isNewLifeCell(currentCellState: Life, liveNeighbours: number) {
+    return currentCellState === Life.Dead && liveNeighbours === 3;
+  }
 
-    liveNeighbours(row: number, col: number) {
-        let liveNeighbours = 0;
+  private isHealthyCell(currentCellState: Life, liveNeighbours: number) {
+    return (
+      currentCellState === Life.Alive &&
+      (liveNeighbours === 2 || liveNeighbours === 3)
+    );
+  }
 
-        for (let i: number = row - 1; i <= row + 1; i++) {
-            for (let j: number = col - 1; j <= col + 1; j++) {
-                if (!(i === row && j === col)) {
-                    if (this.infiniteSpace) {
-                        const r = this.indexOnPlane(i, this.height);
-                        const c = this.indexOnPlane(j, this.width);
-                        liveNeighbours += this.board[r][c];
-                    } else if (this.isWithinPlane(i, j)) {
-                        liveNeighbours += this.board[i][j];
-                    }
-                }
-            }
-        }
+  private indexOnPlane(index: number, planeSize: number) {
+    return index < 0
+      ? index + planeSize
+      : index >= planeSize
+        ? index - planeSize
+        : index;
+  }
 
-        return liveNeighbours;
-    }
-
-    private indexOnPlane(index: number, planeSize: number) {
-        return index < 0 ? index + planeSize : (index >= planeSize ? index - planeSize : index);
-    }
-
-    private isWithinPlane(i: number, j: number) {
-        return i >= 0 && i < this.height && j >= 0 && j < this.width;
-    }
-
-    empty() {
-        const board: Life[][] = new Array(this.height);
-
-        for (let i = 0; i < this.height; i++) {
-            board[i] = new Array<Life>(this.width).fill(Life.Dead);
-        }
-
-        return board;
-    }
-
-    clear() {
-        this.board = this.empty();
-    }
+  private isWithinPlane(i: number, j: number) {
+    return i >= 0 && i < this.height && j >= 0 && j < this.width;
+  }
 }
